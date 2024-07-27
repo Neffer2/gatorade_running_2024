8653e27b7978b3d6e62bd8303df1468d40c334dd
Neffer2 <n3f73r@gmail.com>
2024-07-26 19:08:33 -0500

    1. Corrección enlaces paginación

--- resources/views/livewire/registro/list-registro.blade.php
+++ resources/views/livewire/registro/list-registro.blade.php
@@ -35,6 +35,7 @@
                         </tbody>
                     </table>
                 </div>
+                {{ $corredores->links() }}
             </div>
         </div> 
     </div> 
