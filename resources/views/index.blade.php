a33fd65234d0da4adb9caa93dadac3850f94b604
Neffer2 <n3f73r@gmail.com>
2024-07-26 19:01:01 -0500

    1. Avances Landing mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/css/bootstrap.min.css') }}" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/css/styles.css') }}">
     <title>Inicio</title>
 </head>
 <body>
--- resources/views/livewire/recuerdos/buscar-recuerdo.blade.php
+++ resources/views/livewire/recuerdos/buscar-recuerdo.blade.php
@@ -1,12 +1,33 @@
 <div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <input id="documento" type="text" class="form-control" placeholder="documento" wire:model.change="documento">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="navbrand-container">
+            <img src="{{ asset('assets/Logo_Team_GatoradePersonajes.png') }}" alt="">
+        </div>
+    </nav>
+    <div class="hero">
+        <img src="{{ asset('assets/SigueSudando_1@2x.png') }}" alt="">
+    </div>
+    <div class="form-container">
+        <p>INGRESA Y ENCUENTRA <b>TU FOTO</b></p>
+        <div class="card">
+            <img src="{{ asset('assets/Rayo.svg') }}" class="rayos">
+            <div class="card-header">
+                C&Eacute;DULA
             </div>
-            <div class="col-md-2">
-                <button class="btn btn-primary" wire:click="getRecuerdo">Buscar</button>
+            <div class="card-body">
+                <div class="row gy-4 justify-content-center">
+                    <div class="col-md-4">
+                        <input id="documento" type="text" class="form-control" placeholder="C.C" wire:model.change="documento">
+                    </div>
+                    <div class="col-md-2 d-flex justify-content-center" style="position: relative">
+                        <img src="{{ asset('assets/Buscar.svg') }}" height="15" style="position: absolute; left: 25%; bottom: 25%;">
+                        <button class="btn btn-primary" wire:click="getRecuerdo">BUSCAR</button>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="d-flex justify-content-end">
+            {{-- <img src="{{ asset('assets/Logo_Team_GatoradePersonajes.png') }}" alt=""> --}}
         </div>
     </div>
 </div>
