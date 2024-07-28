c1600905f8776da65f994e7cf09260447f4b5c66
Neffer2 <n3f73r@gmail.com>
2024-07-27 20:50:02 -0500

    1. Se añade favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="https://gatorade.lat/co/wp-content/media/sites/2/2021/04/cropped-favicons-32x32.png" sizes="32x32" />
+    <link rel="icon" href="https://gatorade.lat/co/wp-content/media/sites/2/2021/04/cropped-favicons-192x192.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://gatorade.lat/co/wp-content/media/sites/2/2021/04/cropped-favicons-180x180.png" />
     <link href="{{ asset('css/css/bootstrap.min.css') }}" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/css/styles.css') }}">
     <title>Inicio</title>
